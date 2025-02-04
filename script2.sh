f7023e4a25d247991be1ce9c1b8362c13c3431d1
NagisaKaori <118795794+NagisaKaori@users.noreply.github.com>
2025-02-04 10:19:18 +0300

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -4,7 +4,6 @@ USER_COUNT=10
 
 for i in $(seq 1 $USER_COUNT); do
 	password=$(mysql -N -u Nagi -p'zxc' -e "USE BD; SELECT password FROM Users where username='user$i';")
-	#password="${password/password /}"
 echo $password
 
 	temp_pw=$(echo $password | openssl enc -aes-256-cbc -md sha512 -a -pbkdf2 -iter 10000 -salt -pass pass:"meow") 
