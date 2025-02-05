54d67a04560567f945e6b37c9cdb88c029019ad8
NagisaKaori <118795794+NagisaKaori@users.noreply.github.com>
2025-02-05 09:53:34 +0300

    Update script4.sh

--- script4.sh
+++ script4.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
 mysqldump -u Nagi -pzxc -h localhost --all-databases > "/home/zxc/Desktop/all_databases_backup_$(date +"%Y%m%d_%H%M%S").sql"
-
-if [ $? -eq 0 ]; then
-    echo "Резервное копирование всех баз данных успешно выполнено."
-else
-    echo "Ошибка при резервном копировании всех баз данных."
-fi
