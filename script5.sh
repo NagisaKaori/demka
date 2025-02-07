09cca919d25c762924dfa5a66437c418a40feec8
NagisaKaori <118795794+NagisaKaori@users.noreply.github.com>
2025-02-07 11:03:19 +0300

    Update script5.sh

--- script5.sh
+++ script5.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-mysql -u Nagi -p'zxc' -h localhost < "/home/zxc/Desktop/all_databases_backup_20250204_110738.sql";
+backup_dir="/home/zxc/Desktop"
+latest_backup=$(ls -t "$backup_dir"/*.sql | head -n 1)
 
-#eto 2nd version mb not working, requires additional testing
-##!/bin/bash
-
-#backup_dir="/home/zxc/Desktop"
-#latest_backup=$(ls -t "$backup_dir"/*.sql | head -n 1)
-
-#mysql -u Nagi -p'zxc' -h localhost < "$latest_backup"
+mysql -u Nagi -p'zxc' -h localhost < "$latest_backup"
