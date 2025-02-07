e97b9fa8810b81ffb8950c1f4b162e60a3bf7a07
NagisaKaori <118795794+NagisaKaori@users.noreply.github.com>
2025-02-07 10:13:19 +0300

    Update script4.sh

--- script4.sh
+++ script4.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mysqldump -u Nagi -pzxc -h localhost --all-databases > "/home/zxc/Desktop/all_databases_backup_$(date +"%Y%m%d_%H%M%S").sql"
+mysqldump -u Nagi -p'zxc' -h localhost --all-databases > "/home/zxc/Desktop/all_databases_backup_$(date +"%Y%m%d_%H%M%S").sql";
