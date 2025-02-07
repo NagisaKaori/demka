e00995a34d96b82acfa4b928392b509ceebba248
NagisaKaori <118795794+NagisaKaori@users.noreply.github.com>
2025-02-07 10:13:28 +0300

    Update script5.sh

--- script5.sh
+++ script5.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mysql -u Nagi -p'zxc' -h localhost < "/home/zxc/Desktop/all_databases_backup_20250204_110738.sql"
+mysql -u Nagi -p'zxc' -h localhost < "/home/zxc/Desktop/all_databases_backup_20250204_110738.sql";
